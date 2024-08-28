3765ddfcaf81bfe2f975bb5cb9a62635b99ebbc7
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2024-08-28 12:38:02 +0530

    Update requirements.txt and app.py

--- app.py
+++ app.py
@@ -13,3 +13,63 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# Load the API keys of OpenAI and Groq
+os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+groq_api_key=os.getenv('GROQ_API_KEY')
+
+st.title("Groq with Llama3")
+
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
+
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+"""
+)
+
+def vector_embedding():
+
+    if "vectors" not in st.session_state:
+    
+        st.session_state.embeddings=OpenAIEmbeddings()
+        st.session_state.loader=PyPDFDirectoryLoader("./pdfs") ## Data Ingestion
+        st.session_state.docs=st.session_state.loader.load() ## Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
+
+
+
+
+prompt1=st.text_input("Enter Your Question From Doduments")
+
+
+
+if st.button("Documents Embedding"):
+    vector_embedding()
+    st.write("Vector Store DB is Ready")
+
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever=st.session_state.vectors.as_retriever()
+retrieval_chain=create_retrieval_chain(retriever,document_chain)
+
+if prompt1:
+    start = time.process_time()
+    response=retrieval_chain.invoke({"input":prompt1})
+    print("Response time: ", time.process_time() - start)
+    st.write(response["answer"])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
